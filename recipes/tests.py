1338a6419d54ed06310986545a7f32726362d695
Thiago José Barbosa Lima <thiagojb12@gmail.com>
2022-05-24 17:10:27 -0300

    testing resolve/reverse

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+
+from recipes import views
 
 
 class RecipeURLsTest(TestCase):
@@ -14,3 +16,9 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse("recipes:recipe", kwargs={"id": 1})
         self.assertEquals(url, "/recipes/1/")
+
+
+class RecipeViewTest(TestCase):
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve('/')
+        self.assertIs(view.func, views.home)
