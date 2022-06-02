06af73a66a6f251db44b7475a7c5e0e1ba4bc9d8
Thiago José Barbosa Lima <thiagojb12@gmail.com>
2022-06-02 17:18:28 -0300

    teste model

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -9,6 +9,6 @@ class RecipeModelTest(RecipeTestBase):
         return super().setUp()
 
     def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
-        self.recipe.title = 'A' * 3
+        self.recipe.title = 'A' * 70
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()  # Aqui a validação ocorre
