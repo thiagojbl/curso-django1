101115a33815270685701b5c12f266336cd7b790
Thiago José Barbosa Lima <thiagojb12@gmail.com>
2022-06-08 16:48:03 -0300

    Adicionar paginação CSS

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from utils.pagination import make_pagination_range
 
 from recipes.models import Recipe
 
@@ -17,15 +18,26 @@ from recipes.models import Recipe
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by("-id")
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
 
-    current_page = request.GET.get('page', 1)
-    paginator = Paginator(recipes, 9)
+    paginator = Paginator(recipes, 3)
     page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(
         request,
         "recipes/pages/home.html",
         context={
             "recipes": page_obj,
+            "pagination_range": pagination_range,
         },
     )
 
