65e7900b0d98aea7d537c5ce5e66948a9cd486dd
Thiago José Barbosa Lima <thiagojb12@gmail.com>
2022-06-22 13:45:48 -0300

    método para validar um campo

--- authors/forms.py
+++ authors/forms.py
@@ -1,10 +1,49 @@
-from cProfile import label
-
+from click import password_option
 from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username2')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+        add_attr(self.fields['username'], 'css', 'a-css-class')
+
+    # sobrescrever campo
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least ondeuppercase letter, '
+            'one lowercase letter and one number.'
+        )
+    )
+
+    # criando novo campo
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        })
+    )
+
     class Meta:
         model = User
         # fields = '__all__'
@@ -17,9 +56,54 @@ class RegisterForm(forms.ModelForm):
         ]
 
         labels = {
-            'first_name': 'Username',
-            'last_name': 'First name',
-            'username': 'Last name',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'User-name',
             'email': 'E-mail',
             'password': 'Passwword',
         }
+
+        help_texts = {
+            'email': 'The e-mail must be valid',
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'The field must not be empty',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here.',
+            }),
+
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Type your last name here.',
+            }),
+
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here',
+            })
+        }
+
+    # método para validar um campo
+
+    # def clean_password(self):
+    #     data = self.cleaned_data.get('password')
+    #     if 'atenção' in data:
+    #         raise forms.ValidationError(
+    #             'Não digite %(erro)s no campo password',
+    #             code='ivalid',
+    #             params={'erro': '"atenção"'}
+    #         )
+    #     return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+        if password != password2:
+            raise forms.ValidationError(
+                'Password and password2 must be equal'
+            )
